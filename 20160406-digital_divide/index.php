fdb99f8467b78a8e5fe9ea49950b27bfd0a7ac99
Jef(Frey) Hargarten <jeff.hargarten@startribune.com>
2016-02-10 22:46:03 -0600

    digital divide updates

--- 20160406-digital_divide/index.php
+++ 20160406-digital_divide/index.php
@@ -14,30 +14,43 @@
   <link href="../_common_resources/charts/nvd3-master/build/nv.d3.css" rel="stylesheet" type="text/css">
   
   <style>
-    #map { width:100%; height:350px; }
+    #map { width:100%; height:420px; }
 
-    #nbName { text-align:center; font-family:"Poynter Serif RE"; font-size:1.3em; font-weight:900; }
+    #nbName { font-family:"Poynter Serif RE"; font-size:1.3em; font-weight:900; }
 
     .zoom { text-align:center; float:none !important; padding:15px; }
     .legends { width:280px; height:auto; text-align:center; margin-right:auto; margin-left:auto; }
 
     small { font-family:"Benton Sans",Helvetica,Arial,sans-serif; color:#808080; clear:both; display:block; }
 
+    .switch { padding:10px; display:inline-block; text-align:center; width:49%; background-color:#fff; font-weight:900; font-family:"Benton Sans",Helvetica,Arial,sans-serif; border:1px solid black; }
+    .switch:hover, .selected { background-color:#4C4C39; color:#fff !important; cursor:pointer; }
+
     .num { font-weight:900; }
+
+    #leftSide { float:left; width:48%; }
+    #rightSide { float:right; width:48%; }
 
     .source { text-align:center; }
 
-    #chart { height:430px; }
+    #chart { height:460px; }
+
+    @media (max-width:850px) {
+    #leftSide, #rightSide { width:100%; float:none; text-align:center; }
+    #nbName{ text-align:center; }
+    }
   </style> 
 </head>
 
 <body>
   <div id="wrapper">
 
+<div id="nbName">Minneapolis</div>
+
+<div id="leftSide">
     <div id="map"></div>
 
     <div id="infobox">
-      <div id="nbName">Minneapolis</div>
       <div class="zoom">Reset View</div>
     </div>
 
@@ -58,12 +71,23 @@
     </div>
     <small>No Home Internet Connection</small>
   </div>
+</div>
+
+<div id="rightSide">
+
+ <div id="viewSelect">
+<div class="switch selected" data="house" id="y2014">2014</div>
+<div class="switch" data="senate" id="y2012">2012</div>
+</div>
 
   <div id="chart"><svg></svg></div>
+</div>
 
-  <div class="source">Source: City of Minneapolis</div>
+<div class="breaker"></div>
 
-<a href="http://www.minneapolismn.gov/it/inclusion/WCMS1P-118865" target="new_"><button class="downloadButton">&nbsp;Download data</button></a>
+  <div class="source"><a href="http://www.minneapolismn.gov/it/inclusion/WCMS1P-118865" target="new_">Source: City of Minneapolis</a></div>
+
+<a href="https://docs.google.com/spreadsheets/d/1J8ZCg7kqs89l2JDt2052cZsrF2yoA7bYj15n0ma8DhA/pub?output=xlsx" target="new_"><button class="downloadButton">&nbsp;Download data</button></a>
   </div>
 
 
@@ -89,23 +113,59 @@
 
 //https://script.google.com/macros/s/AKfycbwG7mX6qPZaIhkwY2AJ2lU7kNarbm6OWIkWVfnmYZGYruIl40cu/exec?id=1J8ZCg7kqs89l2JDt2052cZsrF2yoA7bYj15n0ma8DhA&sheet=surveyIsolate
 
-<?php
+
+// d3.json("https://script.googleusercontent.com/macros/echo?user_content_key=S218ArONKeiEz-RTEl53EaGH816h0BB3kYZ0jVrYhx3G0W-toZ_EgLvapvvphJRw-MZjcn2o8wbUygJ6doR4DRmiKO3_m2pgOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHaxCoMjMSmZWLp6XAShvjQj50JtCfh4yP7n1RnEoDeOH7XqmOXgX8RYIyMAhIAtjnF9UDzNXGLr6TDKmXXMxaxxYwQuY5LYBij5AHEx7MO3NlZ2a6-1TkrF_Oz4qafQOPPWDxbiqlNcUVzd_dgT7OAibyRO9NbpYRtymt8WREOXpG&lib=MVcLnEUipyThKZcpmQKyqT_CoSfd4egCX", function(error, json) {
+// d3.json("https://script.googleusercontent.com/macros/echo?user_content_key=xGOPLkfQe4D4nIjoThg4Fn83VyRl1QYzw3bbvxkNTlPFvRQBRNsOFPwGOkGw_V8iYZh_PEg37hJ4qnW1GZT5TLoeuE4A0N21OJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHaxCoMjMSmZWLp6XAShvjQj50JtCfh4yP7n1RnEoDeOH7XqmOXgX8RYIyMAhIAtjnF9UDzNXGLr6TDKmXXMxaxxYwQuY5LYBij5AHEx7MO3NlZ2a6-1TkrF_Oz4qafQOPPWDxbiqlNcUVzd_dgT7OAibyRO9NbpYRt8_pbH_p7GuL&lib=MVcLnEUipyThKZcpmQKyqT_CoSfd4egCX", function(error, json2012) {
+
+$.getJSON('shapefiles/minneapolis_groups.json', function(dataMPLS) {
+
+<?php 
 
 $jsonData = file_get_contents("https://script.googleusercontent.com/macros/echo?user_content_key=S218ArONKeiEz-RTEl53EaGH816h0BB3kYZ0jVrYhx3G0W-toZ_EgLvapvvphJRw-MZjcn2o8wbUygJ6doR4DRmiKO3_m2pgOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHaxCoMjMSmZWLp6XAShvjQj50JtCfh4yP7n1RnEoDeOH7XqmOXgX8RYIyMAhIAtjnF9UDzNXGLr6TDKmXXMxaxxYwQuY5LYBij5AHEx7MO3NlZ2a6-1TkrF_Oz4qafQOPPWDxbiqlNcUVzd_dgT7OAibyRO9NbpYRtymt8WREOXpG&lib=MVcLnEUipyThKZcpmQKyqT_CoSfd4egCX");
+$jsonData2012 = file_get_contents("https://script.googleusercontent.com/macros/echo?user_content_key=xGOPLkfQe4D4nIjoThg4Fn83VyRl1QYzw3bbvxkNTlPFvRQBRNsOFPwGOkGw_V8iYZh_PEg37hJ4qnW1GZT5TLoeuE4A0N21OJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHaxCoMjMSmZWLp6XAShvjQj50JtCfh4yP7n1RnEoDeOH7XqmOXgX8RYIyMAhIAtjnF9UDzNXGLr6TDKmXXMxaxxYwQuY5LYBij5AHEx7MO3NlZ2a6-1TkrF_Oz4qafQOPPWDxbiqlNcUVzd_dgT7OAibyRO9NbpYRt8_pbH_p7GuL&lib=MVcLnEUipyThKZcpmQKyqT_CoSfd4egCX");
 
 ?>
 
 var json = <?php echo $jsonData; ?>;
-
-// d3.json("https://script.googleusercontent.com/macros/echo?user_content_key=S218ArONKeiEz-RTEl53EaGH816h0BB3kYZ0jVrYhx3G0W-toZ_EgLvapvvphJRw-MZjcn2o8wbUygJ6doR4DRmiKO3_m2pgOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHaxCoMjMSmZWLp6XAShvjQj50JtCfh4yP7n1RnEoDeOH7XqmOXgX8RYIyMAhIAtjnF9UDzNXGLr6TDKmXXMxaxxYwQuY5LYBij5AHEx7MO3NlZ2a6-1TkrF_Oz4qafQOPPWDxbiqlNcUVzd_dgT7OAibyRO9NbpYRtymt8WREOXpG&lib=MVcLnEUipyThKZcpmQKyqT_CoSfd4egCX", function(error, json) {
+var json2012 = <?php echo $jsonData2012; ?>;
 
 var data = json.surveyIsolate;
+var data2012 = json2012.surveyIsolate2012;
+
+var dataSource = data;
+
+$(".switch").click(function() {
+  $(".switch").removeClass("selected");
+  $(this).addClass("selected");
+  map.setView([44.974514,-93.268433], zoomification);
+  $('#nbName').html("Minneapolis");
+});
+
+$("#y2012").click(function() {
+dataSource = data2012;
+statesLayer.eachLayer(function (layer) {    
+        layer.setStyle({          
+          fillColor: getColor(layer.feature.properties.GrpName)
+        });  
+      }); 
+redrawChart(chart,"#chart","tech",dataSource,"Overall",0);
+});
+$("#y2014").click(function() {
+dataSource = data;
+statesLayer.eachLayer(function (layer) {    
+        layer.setStyle({          
+          fillColor: getColor(layer.feature.properties.GrpName)
+        });  
+      });
+redrawChart(chart,"#chart","tech",dataSource,"Overall",0);
+});
+
 
 //CHARTAGGEDON
 var chart = [];
 var index = 0;
 // function chartBuild(container,subject,data,county,index){
-  var colorArray = ['#333']; 
+  var colorArray = ['#556E7F']; 
   var formatter = d3.format('%');
 
 nv.addGraph(function() {
@@ -127,7 +187,7 @@ nv.addGraph(function() {
       .tickFormat(formatter);
 
   d3.select('#chart svg')
-      .datum(chartData("tech",data,"Overall"))
+      .datum(chartData("tech",dataSource,"Overall"))
       .transition().duration(500)
       .call(chart[index]);
 
@@ -144,7 +204,7 @@ nv.addGraph(function() {
 // }
 
 function redrawChart(chart,container,subject,data,county,index){
-  var colorArray = ['#333']; 
+  var colorArray = ['#556E7F']; 
   var formatter = d3.format('%');
 
     d3.select(container + ' svg').datum(chartData(subject,data,county)).transition().duration(300).call(chart[index].color(colorArray));
@@ -156,38 +216,38 @@ function chartData(subject,data,county) {
 
 var romney, mccain, huckabee, paul, keyes, guiliani, others, obama, clinton, edwards, kucinich, biden, richardson, dodd, uncommitted;
   
-for (var i=0; i < data.length; i++){
-    if (data[i].group == county){
-      var no_internet = data[i].no_internet;
-      var dialup = data[i].dialup;
-      var cable = data[i].cable;
-      var dsl = data[i].dsl;
-      var satellite = data[i].satellite;
-      var cellular = data[i].cellular;
-      var wifi = data[i].wifi;
-      var unsure = data[i].unsure;
-      var computer = data[i].computer_no_internet + data[i].computer_internet;
-      var tablet = data[i].tablet_internet +  data[i].tablet_no_internet;
-      var cell = data[i].cell_internet + data[i].cell_no_internet;
-      var gaming = data[i].gaming_internet + data[i].gaming_no_internet;
-      var less_highschool = data[i].less_highschool;
-      var highschool = data[i].highschool;
-      var associates_some = data[i].associates_some;
-      var bachelors = data[i].bachelors;
-      var graduate = data[i].graduate;
-      var i_10k_24k = data[i].i_10k_24k + data[i].i_less_10k;
-      var i_25k_50k = data[i].i_25k_50k;
-      var i_50k_99k = data[i].i_50k_75k + data[i].i_75k_100k;
-      var i_100k = data[i].i_100k_150k + data[i].i_150k_200k + data[i].i_more_200k;
-      var native = data[i].native;
-      var asian = data[i].asian;
-      var black = data[i].black;
-      var white = data[i].white;
-      var other = data[i].other;
-      var y_18_34 = data[i].y_18_24 + data[i].y_25_34;
-      var y_35_54 = data[i].y_35_44 + data[i].y_45_54;
-      var y_55_64 = data[i].y_55_64;
-      var y_65 = data[i].y_65_74 + data[i].y_75_older;
+for (var i=0; i < dataSource.length; i++){
+    if (dataSource[i].group == county){
+      var no_internet = dataSource[i].no_internet;
+      var no_mobile = dataSource[i].cell_no_internet;
+      var dialup = dataSource[i].dialup;
+      var cable = dataSource[i].cable;
+      var dsl = dataSource[i].dsl;
+      var satellite = dataSource[i].satellite;
+      var cellular = dataSource[i].cellular;
+      var wifi = dataSource[i].wifi;
+      var unsure = dataSource[i].unsure;
+      var computer = dataSource[i].computer_no_internet + dataSource[i].computer_internet;
+      var tablet = dataSource[i].tablet_internet +  dataSource[i].tablet_no_internet;
+      var cell = dataSource[i].cell_internet + dataSource[i].cell_no_internet;
+      var gaming = dataSource[i].gaming_internet + dataSource[i].gaming_no_internet;
+      var less_highschool = dataSource[i].less_highschool;
+      var highschool = dataSource[i].highschool;
+      var associates_some = dataSource[i].associates_some;
+      var bachelors = dataSource[i].bachelors;
+      var graduate = dataSource[i].graduate;
+      var i_10k_24k = dataSource[i].i_10k_24k + dataSource[i].i_less_10k;
+      var i_25k_50k = dataSource[i].i_25k_50k;
+      var i_50k_99k = dataSource[i].i_50k_75k + dataSource[i].i_75k_100k;
+      var i_100k = dataSource[i].i_100k_150k + dataSource[i].i_150k_200k + dataSource[i].i_more_200k;
+      var native = dataSource[i].native;
+      var asian = dataSource[i].asian;
+      var black = dataSource[i].black;
+      var white = dataSource[i].white;
+      var y_18_34 = dataSource[i].y_18_24 + dataSource[i].y_25_34;
+      var y_35_54 = dataSource[i].y_35_44 + dataSource[i].y_45_54;
+      var y_55_64 = dataSource[i].y_55_64;
+      var y_65 = dataSource[i].y_65_74 + dataSource[i].y_75_older;
   }
 }
 
@@ -195,8 +255,12 @@ for (var i=0; i < data.length; i++){
         "key": "Prevalence",
         "values": [
           { 
-            "label" : "No Internet" ,
+            "label" : "No Home Internet" ,
             "value" : no_internet
+          },
+          { 
+            "label" : "No Mobile Internet" ,
+            "value" : no_mobile
           },
           { 
             "label" : "Dialup Internet" ,
@@ -306,7 +370,6 @@ for (var i=0; i < data.length; i++){
       }]
 }
 
-
 //MAPOCALYPSE
 var zoomification = 11;
 
@@ -325,7 +388,7 @@ map.setView([44.974514,-93.268433], zoomification);
 $(".zoom").click(function() {
   map.setView([44.974514,-93.268433], zoomification);
   $('#nbName').html("Minneapolis");
-  redrawChart(chart,"#chart","tech",data,"Overall",0)
+  redrawChart(chart,"#chart","tech",data,"Overall",0);
 });
 
 //INITIALIZE MAP
@@ -341,8 +404,6 @@ map.dragging.disable();
 map.touchZoom.disable();
 map.doubleClickZoom.disable();
 map.scrollWheelZoom.disable();
-
-$.getJSON('shapefiles/minneapolis_groups.json', function(dataMPLS) {
 
    statesLayer = L.geoJson(dataMPLS,  {
       style: getStyle,
@@ -360,14 +421,14 @@ $.getJSON('shapefiles/minneapolis_groups.json', function(dataMPLS) {
   }
 
     function getColor(d) {
-        for (var i=0; i < data.length; i++){
-          if (d == data[i].group){
-           if (data[i].no_internet >= .20) { return "#2C3942"; }
-           if (data[i].no_internet >= .15) { return "#556E7F"; }
-           if (data[i].no_internet >= .10) { return "#7F98AA"; }
-           if (data[i].no_internet >= .5) { return "#A8B9C5"; }
-           if (data[i].no_internet > 0) { return "#C6D1D9"; }
-           if (data[i].no_internet == 0) { return "#DAE1E7"; }
+        for (var i=0; i < dataSource.length; i++){
+          if (d == dataSource[i].group){
+           if (dataSource[i].no_internet >= .20) { return "#2C3942"; }
+           if (dataSource[i].no_internet >= .15) { return "#556E7F"; }
+           if (dataSource[i].no_internet >= .10) { return "#7F98AA"; }
+           if (dataSource[i].no_internet >= .5) { return "#A8B9C5"; }
+           if (dataSource[i].no_internet > 0) { return "#C6D1D9"; }
+           if (dataSource[i].no_internet == 0) { return "#DAE1E7"; }
         }
       }
   }
@@ -388,9 +449,9 @@ $.getJSON('shapefiles/minneapolis_groups.json', function(dataMPLS) {
 
       popup.setLatLng(e.latlng);
 
-for (var i=0; i < data.length; i++){
-  if (layer.feature.properties.GrpName == data[i].group){
-    var stat = '<div class="num" style="color:' + getColor(layer.feature.properties.GrpName) + ';">' + d3.format("%")(data[i].no_internet) + ' without home Internet</div>'
+for (var i=0; i < dataSource.length; i++){
+  if (layer.feature.properties.GrpName == dataSource[i].group){
+    var stat = '<div class="num" style="color:' + getColor(layer.feature.properties.GrpName) + ';">' + d3.format("%")(dataSource[i].no_internet) + ' without home Internet</div>'
     break;
       }
       else { stat = ""; }
@@ -425,11 +486,11 @@ for (var i=0; i < data.length; i++){
     var layer = e.target;
       map.fitBounds(layer.getBounds());
       $('#nbName').html(layer.feature.properties.GrpName);
-      redrawChart(chart,"#chart","tech",data,layer.feature.properties.GrpName,0)
+      redrawChart(chart,"#chart","tech",dataSource,layer.feature.properties.GrpName,0);
   }
 
   });
-
+ // });
 // });
 </script>
 
