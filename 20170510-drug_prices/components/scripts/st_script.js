37ab01f1b026065ee8c49c1668cf007eba19878b
Frey Hargarten <harga012@umn.edu>
2017-06-20 09:45:03 -0500

    Update st_script.js

--- 20170510-drug_prices/components/scripts/st_script.js
+++ 20170510-drug_prices/components/scripts/st_script.js
@@ -6,6 +6,20 @@
 $(document).ready(function() {
   var allData;
   var currentID;
+  var initialDrug = 'epipen-2-pak';
+
+  // Extra information about notable drugs
+  var extra = {
+    'epipen-2-pak': 'The EpiPen 2 Pak, a common EpiPen product, an epinephine auto-injector used to treat allergy reactions, has seen its price steadily rise since 2011.',
+    'thiola': 'Thiola, an orphan drug, was bought in 2013 by Retrophin, founded by Martin Shkreli, who drastically raised the price.',
+    'daraprim': 'Daraprim, used to fight parasitic infections, acquired in 2014 by Turing Pharmaceuticals, founded by Martin Shkreli, raised the price remarkably.',
+    'chenodal': 'Chenodal is used to treat cerebrotendinous xanthomatosis (CTX), a rare genetic disorder, and saw its price spike in 2014 by Retrophin where Martin Shkreli was the CEO at the time.',
+    'crestor': 'Crestor, used to treat high cholesterol and triglyceride levels, has seen a steady increase from 2011 to 2015.',
+    'humira-humira-pen': 'Humira is used to treat arthritis, Crohn\'s disease, and other illnesses',
+    'ambien': 'Ambien is used to help patients sleep.',
+    'lyrica': 'Lyrica, used to treat nerve and muscle pain including fibromyalgia, has seens its price double from 2011 to 2015.',
+    'lantus-lantus-solostar': 'Lantus is a popularly presecribed medicine to treat diabetes.'
+  };
 
   // Try to get the jquery ui widget to behave better
   // https://stackoverflow.com/questions/5643767/jquery-ui-autocomplete-width-not-set-correctly
@@ -23,12 +37,16 @@ $(document).ready(function() {
 
     // Do any calculations
     allData = rows;
+    allData.map(function(d) {
+      d.extraInfo = extra[d.id] ? extra[d.id] : undefined;
+      return d;
+    });
 
     // Populate search
     populateSearch();
 
     // Draw default chart
-    showDrug('epipen-2-pak');
+    showDrug(initialDrug);
 
     // Handle drug switching buttons
     $('.drug-switch').on('click', function() {
@@ -103,6 +121,9 @@ $(document).ready(function() {
     $container.find('.per-user-full').toggle(!!data.perUserFull);
     $container.find('.not-per-user-full').toggle(!data.perUserFull);
 
+    $container.find('.extra-info').html(data.extraInfo)
+      .toggle(!!data.extraInfo);
+
     // Drug switch
     $('.drug-switch').removeClass('active');
     $('[data-id="' + id + '"]').addClass('active');
@@ -114,7 +135,7 @@ $(document).ready(function() {
         top: 20,
         right: 60,
         bottom: 20,
-        left: 60,
+        left: 90,
       },
       data: {
         x: 'x',
@@ -180,9 +201,17 @@ function urlParam(name) {
 }
 
 // Format money
-function formatCurrency(number, places) {
-  places = places || 2;
-  return '$' + Math.floor(number * Math.pow(10, places)) / Math.pow(10, places);
+// https://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
+function formatCurrency(number, places, d, t) {
+  places = isNaN(places = Math.abs(places)) ? 2 : places;
+  d = d == undefined ? '.' : d;
+  t = t == undefined ? ',' : t;
+
+  var s = number < 0 ? '-$' : '$';
+  var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(places)));
+  var j = (j = i.length) > 3 ? j % 3 : 0;
+
+  return s + (j ? i.substr(0, j) + t : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, '$1'+ t) + (places ? d + Math.abs(number - i).toFixed(places).slice(2) : "");
 }
 
 // Format change
@@ -190,7 +219,7 @@ function formatChange(change, places) {
   places = places || 2;
   return '<span class="' +
     (change > 0 ? 'positive' : change === 0 ? '' : 'negative') + '">' +
-    (change > 0 ? '+' : change === 0 ? '' : '-') +
+    (change > 0 ? '+' : change === 0 ? '' : '') +
     (Math.round(change * Math.pow(10, places)) / Math.pow(10, places)) + '%' +
     '</span>';
 }
