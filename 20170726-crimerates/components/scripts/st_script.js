036c5d02b3a45fce76ac9f1dbd20b94b71fc53a6
Frey Hargarten <jeff.hargarten@startribune.com>
2017-07-25 14:11:23 -0500

    axis fix

--- 20170726-crimerates/components/scripts/st_script.js
+++ 20170726-crimerates/components/scripts/st_script.js
@@ -1,3 +1,4 @@
+//chart selection parameters
 $.urlParam = function(name){
   var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
   if (results != null) { return results[1] || 0; }
@@ -17,6 +18,7 @@ d3.json("./data/ranking.json", function(error, dataLoad) {
 
 var dataRank = dataLoad.ranking;
 
+//build the neighbor crime rate ranking table
 function tableBuild(){
 d3.select("#chart").selectAll(".row")
   .data(dataRank).enter().append("div")
@@ -66,6 +68,7 @@ function tableSort(container,party,data,candidate,sorted){
     .transition().duration(500);
 }
 
+//interface triggers
 $( document ).ready(function() {
  $(".row").click(function() {
    $(".row").removeClass("rowSelect");
@@ -116,6 +119,7 @@ $('.scrollToTop').click(function(){
   });
 });
 
+//crime rate map
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2hhZG93ZmxhcmUiLCJhIjoiS3pwY1JTMCJ9.pTSXx_LFgR3XBpCNNxWPKA';
 
 var map = new mapboxgl.Map({
@@ -224,7 +228,7 @@ map.on('mousemove', function(e) {
 
 });
 
-
+//racial breakdown chart
 function raceChart(){
     var chart = c3.generate({
       bindto: '#raceChart',
@@ -294,6 +298,7 @@ function raceChart(){
 
 raceChart();
 
+//timeline chart
 d3.csv("./data/timeline.csv", function(d) {
   return {
     filed: d.DeathDate,
@@ -325,9 +330,6 @@ for (var i=0; i < data.length; i++){
   if (data[i].code == 10) { unarmedNum[i+1] = data[i].code; allNum[i+1] = null; }
   if (data[i].code == 30) { unarmedNum[i+1] = null; allNum[i+1] = data[i].code; }
 }
-
-console.log(axis);
-console.log(allNum);
 
 var  padding = {
         top: 20,
@@ -363,7 +365,7 @@ chart = c3.generate({
     axis: {
       y: {
             min: 0,
-            padding: {bottom: 0},
+            padding: {bottom: 0, left:0},
             tick: {
              format: function (d) {
                     switch (d) {
@@ -380,7 +382,7 @@ chart = c3.generate({
             type: 'categories',
             categories: ['05-14-2000','07-22-2006','05-12-2012','07-15-2017'],
             tick: {
-                count: 4,
+                count: 2,
                 multiline: false
             }
           }
